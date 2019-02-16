cfc20a4f20a2b3f79175a199dae3e7f435c6a1dd
haggag <tifal760@gmail.com>
2019-02-16 23:52:32 +0200

    the packages needed

--- SpeechRecognizer/sample_taker.py
+++ SpeechRecognizer/sample_taker.py
@@ -1,4 +1,18 @@
 from SpeechRecognizer.recorder import SoundRecorder
 S = SoundRecorder()
 S.record_for_all_categories()
-# S.record_for_one_category("start engine")
+#S.record_for_one_category("start engine")
+#Hey veronica
+# i hope you are well
+#you need to run this file and record for each person
+#make sure that person knows that he need to make his voice very clear and loud in the mic
+#make sure to tell the person to listen to what he is listening to
+#in case you find any errors like " i can not find the class caled recorder"
+#change first line to from recorder import SoundRecorder
+#in case you are running from Linux, a error will occur
+# open recorder file
+#the problem is that in windows you need to have any directory with \\ while in linux mafrood ykoon fi one slash bs
+#yemkn dh tkoon el moshkla lw you run in ubunutu
+# try kda os.path.normpath(path) 3ala el path el gowa recorder
+#or sheel el 2 slasha to 1 slash in  python
+#probability the problem will be in line 15 and line 16
