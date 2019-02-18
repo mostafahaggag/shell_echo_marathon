8240253eb5838cf09e536b713ca975503a27b60f
haggag <tifal760@gmail.com>
2019-02-18 18:28:32 +0200

    the need data I made at the university

--- SpeechRecognizer/sample_taker.py
+++ SpeechRecognizer/sample_taker.py
@@ -7,7 +7,7 @@ S.record_for_all_categories()
 #you need to run this file and record for each person
 #make sure that person knows that he need to make his voice very clear and loud in the mic
 #make sure to tell the person to listen to what he is listening to
-#in case you find any errors like " i can not find the class caled recorder"
+#in case you find any yerrors like " i can not find the class caled recorder"
 #change first line to from recorder import SoundRecorder
 #in case you are running from Linux, a error will occur
 # open recorder file
--- SpeechRecognizer/train.py
+++ SpeechRecognizer/train.py
@@ -2,21 +2,21 @@ from SpeechRecognizer.sound_extractor import SoundExtraction
 from SpeechRecognizer.neural_network import RNN,DNN,plot
 from SpeechRecognizer.recognizer import Recognizer
 from SpeechRecognizer.editing_voice import changing_wav_files
-# y=changing_wav_files()
+y=changing_wav_files()
 # # #you can change min and max and step change in the pitch and the magnitude from the function it self, no need to open the class
 # # #use the parameters of the object like
-# # #y.play_with_pitch(min_pitch=0.3,max_pitch=1,step_pitch=0.2)
+y.play_with_pitch(min_pitch=0.2,max_pitch=1,step_pitch=0.2)
 # y.change_in_magnitude(min_mag=-18,max_mag=18,step_mag=2)
 # # #the min and max and step are used to tell the function how much to change in the signal
 # # #the default parameters are set inside of the class so if you want you may not enter anything
 # S = SoundExtraction()
 # S.create_training_data(type='RNN',pitch=False, magnitude =False)
 # #it is a must to set parameter pitch=true, magnitude =true to use the data which has a different magnitude or frequency
-T = RNN()
-history = T.train()
-plot(history)
-the_dnn_model="Model.h5"
-the_rnn_model="ModelRNN.h5"
-test = Recognizer(model=the_rnn_model)
-# test.start("RNN")
-test.testaccuracy(type="RNN")
+# T = RNN()
+# history = T.train()
+# plot(history)
+# the_dnn_model="Model.h5"
+# the_rnn_model="ModelRNN.h5"
+# test = Recognizer(model=the_rnn_model)
+# # test.start("RNN")
+# test.testaccuracy(type="RNN")
